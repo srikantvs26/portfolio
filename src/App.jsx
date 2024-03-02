df72d43e2380b50f0138b325d634bee91f693c31
Srikant V S <38664247+srikantvs26@users.noreply.github.com>
2024-03-02 15:39:59 +0000

    added navbar and background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import "./App.css";
-import TypeWriter from "./Component/TypeWriter";
-
+import NavBar from "./Component/NavBar/NavBar";
+import TypeWriter from "./Component/TypeWriter/TypeWriter";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
        <TypeWriter/>
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
       </header>
     </div>
   );
