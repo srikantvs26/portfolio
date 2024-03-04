98131a192c8ea584dc996655db2a01ca88ab871d
Srikant V S <38664247+srikantvs26@users.noreply.github.com>
2024-03-04 06:07:03 +0000

    added particles and configured them.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import NavBar from "./Component/NavBar/NavBar";
+import Particle from "./Component/Particle";
 import TypeWriter from "./Component/TypeWriter/TypeWriter";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <NavBar/>
+        <Particle/>
        <TypeWriter/>
       </header>
     </div>
